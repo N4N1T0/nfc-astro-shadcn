a1797fe518cb902913b90ac1e3a2350fc6420007
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-10-16 19:50:20 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://astro-shadcn-ui-template.vercel.app' : 'http://localhost:4321',
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), sitemap()]
+  }), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
