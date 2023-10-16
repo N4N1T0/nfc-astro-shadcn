5408c1842deb1f97d3b0332fd938472551d02282
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-10-17 00:09:04 +0200

    added party/town and fix site at astro-config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://astro-shadcn-ui-template.vercel.app' : 'http://localhost:4321',
+  site: process.env.CI ? 'https://nfc-astro-shadcn.vercel.app' : 'http://localhost:4321',
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), sitemap()],
+  }), sitemap(), partytown()],
   output: "server",
   adapter: vercel()
 });
