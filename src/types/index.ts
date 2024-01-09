1c1748df8e3389dd870a59cce79bc0c2a972a873
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-01-09 01:26:17 +0100

    Add Pagination to the blog Index

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export interface Page<T = any> {
+  /** result */
+  data: T[];
+  /** metadata */
+  /** the count of the first item on the page, starting from 0 */
+  start: number;
+  /** the count of the last item on the page, starting from 0 */
+  end: number;
+  /** total number of results */
+  total: number;
+  /** the current page number, starting from 1 */
+  currentPage: number;
+  /** number of items per page (default: 25) */
+  size: number;
+  /** number of last page */
+  lastPage: number;
+  url: {
+    /** url of the current page */
+    current: string;
+    /** url of the previous page (if there is one) */
+    prev: string | undefined;
+    /** url of the next page (if there is one) */
+    next: string | undefined;
+  };
+}
