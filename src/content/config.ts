15e2ba781d031c8bf7ef8f8dd2b2eda404bd4667
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-04-14 23:12:21 +0200

    feat: Add clients content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 
 import { z, defineCollection } from 'astro:content';
 
-// Typescript for the Blog Content
+// Typescript validation for the Blog Content
 const blogCollection = defineCollection({ type: 'content', 
   schema: ({ image }) => z.object({
     title: z.string().max(60, "For optimze SEO, please provide a title with 60 characters or less"),
@@ -11,8 +11,18 @@ const blogCollection = defineCollection({ type: 'content',
     categories: z.array(z.string()),
     image: image(),
     imageAlt: z.string()
-  }), });
+  }), })
+
+  // Typescript validation for the Client Content
+  const clientsCollection = defineCollection({ type: 'content',
+    schema: () => z.object({
+      title: z.string().max(60, "For optimze SEO, please provide a title with 60 characters or less"),
+      excerpt: z.string().max(160, "For optimze SEO, please provide a excerpt/description with 160 characters or less"),
+      date: z.date(),
+    }),
+  })
 
 export const collections = {
   'blog': blogCollection,
+  'clients': clientsCollection
 };
